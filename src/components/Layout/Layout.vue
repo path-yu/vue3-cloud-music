687b0eb64058e442b49159e027234b5cd293ee02
path-yu <1974675011@qq.com>
2022-04-01 18:23:51 +0800

    更新文件名

---
+++ src/components/Layout/Layout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import BaseHeader from '@/components/Layout/components/LayoutHeader.vue';
+import LayoutLeftMenu from '@/components/Layout/components/LayoutLeftMenu.vue';
+</script>
+
+<template>
+  <div :class="['transition-colors','duration-300']">
+    <base-header />
+    <layout-left-menu />
+  </div>
+</template>
+
+<style scoped>
+</style>
