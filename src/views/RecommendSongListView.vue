0caf0715a1c275ef725b0dc9718014c89551568f
path-yu <1974675011@qq.com>
2022-04-03 10:04:55 +0800

    添加loading

--- src/views/RecommendSongListView.vue
+++ src/views/RecommendSongListView.vue
@@ -9,25 +9,30 @@ const { state: banners, isLoading } = useAsyncState(getBanner().then(res => res.
 
 <template>
   <div>
-    <n-carousel
-      effect="card"
-      draggable
-      prev-slide-style="transform: translateX(-150%) translateZ(-800px);"
-      next-slide-style="transform: translateX(50%) translateZ(-800px);"
-      style="height: 250px"
-      :show-dots="true"
+    <n-spin
+      :show="isLoading"
+      description="载入中"
     >
-      <n-carousel-item
-        v-for="item in banners"
-        :key="item.imageUrl"
-        :style="{ width: '50%' }"
+      <n-carousel
+        effect="card"
+        draggable
+        prev-slide-style="transform: translateX(-150%) translateZ(-800px);"
+        next-slide-style="transform: translateX(50%) translateZ(-800px);"
+        style="height: 250px"
+        :show-dots="true"
       >
-        <img
-          class="w-full h-full rounded"
-          :src="item.imageUrl"
+        <n-carousel-item
+          v-for="item in banners"
+          :key="item.imageUrl"
+          :style="{ width: '50%' }"
         >
-      </n-carousel-item>
-    </n-carousel>
+          <img
+            class="w-full h-full rounded cursor-pointer"
+            :src="item.imageUrl"
+          >
+        </n-carousel-item>
+      </n-carousel>
+    </n-spin>
   </div>
 </template>
 
