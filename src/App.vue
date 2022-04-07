3e462bbdcd54a5747871171521a3993f3938b241
path-yu <1974675011@qq.com>
2022-04-07 11:18:26 +0800

    初始化加载主题

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import LayOut from '@/components/Layout/Layout.vue';
+import { onMounted } from 'vue';
 import { useMainStore } from './stores/main';
 
 const mainStore = useMainStore();
 
+onMounted(() => {
+  mainStore.initDocumentTheme();
+});
 </script>
 <template>
   <n-config-provider :theme="mainStore.activeTheme">
