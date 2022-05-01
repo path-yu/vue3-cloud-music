6664fc5142d43097a378bc40c6cbc86d33b24a96
path-yu <1974675011@qq.com>
2022-05-01 22:22:01 +0800

    格式化数字方法优化

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,13 @@
 
 // 格式化数字
 export function formateNumber(num:number) {
+  // 小于999的数字直接返回
   if (num < 99999) return num.toString();
-  if (num >= 10000 && num < 1000000000) {
-    return Math.round(num / 10000) + '万';
-  }
-  if (num >= 1000000000) {
-    return Math.round(num / 1000000000) + '亿';
-  }
+  // 大于1万 小于1亿
+  if (num < 100000000) return Math.round(num / 10000) + '万';
+  // 大于1亿
+  if (num >= 100000000) return Math.round(num / 100000000) + '亿';
+ 
   return num.toString();
 }
 
