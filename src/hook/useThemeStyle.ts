67ffa4b482f1b78192d31cb8fdda577a6da62534
path-yu <1974675011@qq.com>
2022-06-26 10:55:38 +0800

    feat: render search playlist

--- src/hook/useThemeStyle.ts
+++ src/hook/useThemeStyle.ts
@@ -29,5 +29,16 @@ export default function useThemeStyle(otherStyle?:CSSProperties) {
   const primaryColor = computed(() => {
     return themeVars.value.primaryColor;
   });
-  return { tableStripedStyle, themeVars, tagColor, baseColor, primaryColor };
+  const stripedClass = computed(() => {
+    return (index:number) => {
+      let classes = '';
+      if (isEven(index)) {
+        classes = 'bg-white hover:bg-gray-100 dark:bg-black/50 dark:hover:bg-gray-200/10';
+      } else {
+        classes = 'bg-gray-50 hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-200/10';
+      }
+      return classes;
+    };
+  });
+  return { tableStripedStyle, themeVars, tagColor, baseColor, primaryColor, stripedClass };
 }
--- src/views/search/SearchResult.vue
+++ src/views/search/SearchResult.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { search, type SearchParams } from '@/service';
+import { PlayCircleOutlined } from '@vicons/antd';
 import { useMainStore } from '@/stores/main';
 import { useAsyncState } from '@vueuse/core';
-import { watch, computed, type Ref, reactive, ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { useThemeVars } from 'naive-ui';
+import { formateNumber } from '@/utils';
+import { watch, computed, type Ref, reactive, ref, type CSSProperties } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import useThemeStyle from '@/hook/useThemeStyle';
 
 let immediateCall = false;
+let backTopEle:HTMLElement;
 const playListPageParams = reactive({
   pageCount: 0,
   page: 1,
@@ -16,68 +21,67 @@ const songListPageParams = reactive({
   page: 1,
   pageSize: 50
 });
-const tab = ref('playlist');
+const currentTabIndex = ref(0);
+const router = useRouter();
 const route = useRoute();
 const mainStore = useMainStore();
+const { themeVars, stripedClass } = useThemeStyle();
 const { state: songsSearchResult, isLoading: songListIsLoading, execute: getSearchSongList } = useAsyncState(
   (val) => search(val).then(async res => {
     let result = res.data.result;
     res.data.result.songs = mainStore.mapSongListAddLike(result.songs);
     songListPageParams.pageCount = Math.round(result.songCount / songListPageParams.pageSize) || 1;
     return res.data.result;
-  }), {}, { resetOnExecute: false, immediate: false }
+  }), {}, { resetOnExecute: true, immediate: false }
 );
 const { state: playListSearchResult, isLoading: playListIsLoading, execute: getSearchPlayList } = useAsyncState(
   (val) => search(val).then(async res => {
     let result = res.data.result;
     playListPageParams.pageCount = Math.round(result.playlistCount / playListPageParams.pageSize) || 1;
     return result;
-  }), {}, { resetOnExecute: false, immediate: false }
+  }), {}, { resetOnExecute: true, immediate: false }
 );
-const tabName = (
-  baseName:string, hasValue:Ref<any>, key:string
-) => {
-  if (hasValue.value[key]) {
-    return `${baseName}（找到${hasValue.value[key]}首${baseName}）`;
-  }
-  return baseName;
+
+const activeTabStyle:(index:number) => CSSProperties = (index:number) => {
+  if (index === currentTabIndex.value) {
+    return {
+      color: themeVars.value.primaryColor,
+      borderBottom: `2px solid ${themeVars.value.primaryColor}`
+    };
+  } 
+  return {};
 };
-const singSongTabName = computed(() => tabName(
-  '单曲', songsSearchResult, 'songCount'
-));
-const playListTabName = computed(() => tabName(
-  '歌单', playListSearchResult, 'playlistCount'
-));
+
 watch(
   [() => route.query, () => playListPageParams.page, () => playListPageParams.pageSize], () => {
    
-    let baseParams:SearchParams = {
+    let songParams:SearchParams = {
       limit: 30,
       keywords: route.query.keyword as string,
       type: '1',
       offset: 0
     };
     let playListParams = {
-      ...baseParams,
+      keywords: route.query.keyword as string,
       type: '1000',
       limit: playListPageParams.pageSize,
       offset: ((playListPageParams.page) - 1) * playListPageParams.pageSize
     };
     if (!immediateCall) {
       getSearchSongList(
-        3000, baseParams
+        0, songParams
       );
       getSearchPlayList(
-        3000, playListParams
+        0, playListParams
       );
     } else {
-      if (tab.value === 'song') {
+      if (currentTabIndex.value === 0) {
         getSearchSongList(
-          3000, baseParams
+          0, songParams
         );
       } else {
         getSearchPlayList(
-          3000, playListParams
+          0, playListParams
         );
       }
     }
@@ -88,38 +92,84 @@ watch(
   },
   { immediate: true }
 );
+watch(
+  [playListPageParams, songListPageParams], () => {
+    backTopEle = document.querySelector('.n-back-top') as HTMLElement;
+    backTopEle && backTopEle.click();
+  }
+);
 </script>
 
 <template>
-  <div class="p-4">
-    <h2>搜索 {{ route.query.keyword }}</h2>
-    <n-tabs v-model:value="tab" type="line" animated>
-      <n-tab-pane name="song" :tab="singSongTabName">
-        <n-spin :show="songListIsLoading" description="搜索中">
-          <div v-show="songListIsLoading" class="h-80" />
-          歌曲
-        </n-spin>
-      </n-tab-pane>
-      <n-tab-pane name="playlist" :tab="playListTabName">
-        <n-spin :show="playListIsLoading" description="搜索中">
-          <div v-show="playListIsLoading" class="h-80" />
-          歌单
-          <!-- 分页 -->
-          <div v-if="playListPageParams.pageCount > 1" class="flex justify-center mt-6">
-            <n-pagination
-              v-model:page="playListPageParams.page" 
-              v-model:page-size="playListPageParams.pageSize" 
-              :page-count="playListPageParams.pageCount" 
-              show-size-picker
-              :page-sizes="[10, 20, 30, 40,50]"
-            />
-          </div>
-        </n-spin>
-      </n-tab-pane>
-    </n-tabs>
+  <div>
+    <h2 class="pl-8">
+      搜索 {{ route.query.keyword }}
+    </h2>
+    <div class="flex px-4">
+      <div :style="activeTabStyle(0)" class="px-4  pb-2 opacity-80 hover:opacity-100 transition-opacity cursor-pointer" @click="currentTabIndex = 0">
+        单曲
+      </div>
+      <div :style="activeTabStyle(1)" class="px-4 pb-2 ml-4 hover:opacity-100 transition-opacity cursor-pointer" @click="currentTabIndex = 1">
+        歌单
+      </div>
+    </div>
+    <div v-show="currentTabIndex === 0">
+      <n-spin :show="songListIsLoading" description="搜索中">
+        <div v-show="songListIsLoading" class="h-80" />
+        歌曲
+      </n-spin>
+    </div>
+    <div v-show="currentTabIndex === 1">
+      <n-spin :show="playListIsLoading" description="搜索中">
+        <div v-show="playListIsLoading" class="h-80" />
+        <p v-if="playListSearchResult.playlistCount" class="pl-8 mt-4 mb-2 opacity-50">
+          共找到{{ playListSearchResult.playlistCount }}个歌单
+        </p>
+        <div
+          v-for="(item,index) in playListSearchResult.playlists"
+          :key="item.id" :class="'flex items-center py-4 px-8 cursor-pointer ' + stripedClass(index)" @click="router.push(`/songList/${item.id}`)"
+        >
+          <load-img
+            loading-height="64px"
+            class-name="w-16 h-16 rounded-md"
+            :src="item.coverImgUrl"
+          />
+          <n-ellipsis :tooltip="false" class="pl-2 w-80">
+            {{ item.name }}
+          </n-ellipsis>
+          <p class="w-16 opacity-50">
+            {{ item.trackCount }}首
+          </p>
+          <p class="w-80">
+            <span class="opacity-50">by </span> 
+            <span class="opacity-80">{{ item.creator.nickname }}首</span>
+          </p>
+          <p class="w-80 opacity-50">
+            <n-icon :component="PlayCircleOutlined" />
+            {{ formateNumber(item.playCount) }}
+          </p>
+        </div>
+        
+        <!-- 分页 -->
+        <div v-if="playListPageParams.pageCount > 1" class="flex justify-center my-6">
+          <n-pagination
+            v-model:page="playListPageParams.page" 
+            v-model:page-size="playListPageParams.pageSize" 
+            :page-count="playListPageParams.pageCount" 
+            show-size-picker
+            :page-sizes="[10, 20, 30, 40,50]"
+          />
+        </div>
+      </n-spin>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+:deep(.n-tabs .n-tab-pane){
+  padding:0;
+}
+:deep(.n-tabs-tab-wrapper){
+  padding:0 20px;
+}
 </style>
