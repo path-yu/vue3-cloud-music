35906da7e2bebf7c41d9de6bd50d476806889648
path-yu <1974675011@qq.com>
2023-04-08 14:36:40 +0800

    fix: english input method not working

--- src/components/Player/FooterPlayer.vue
+++ src/components/Player/FooterPlayer.vue
@@ -250,7 +250,7 @@ const handlePlayModeClick = () => {
 };
 // 点击空格播放
 const handlePressSpace = (e:KeyboardEvent) => {
-  e.preventDefault();
+  // e.preventDefault();
   if (e.code === 'Space' && mainStore.currentPlaySong) {
     togglePlayStatus();
   }
