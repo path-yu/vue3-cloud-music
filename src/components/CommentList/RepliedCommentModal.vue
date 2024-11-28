54d7d02277be8ac298a799355c5ab7881015863a
path-yu <1974675011@qq.com>
2024-11-28 18:55:18 +0800

    update tsconfig

--- src/components/CommentList/RepliedCommentModal.vue
+++ src/components/CommentList/RepliedCommentModal.vue
@@ -6,13 +6,13 @@ const showModal = ref(false);
 const commentBtnLoading = ref(false);
 const commentContent = ref('');
 const props = withDefaults(defineProps<{
-  commentPlaceholder?:string;
-  title:string;
-  resourceId:number;//资源id
-  type?:number;//资源类型0: 歌曲 1: mv2: 歌单3: 专辑4: 电台5: 视频6: 动态
-  commentId?:number;
-  t?:number;//评论类型1: 发送 2: 回复
-  updateCommentList:(comment:any)=>void;
+  commentPlaceholder?: string;
+  title: string;
+  resourceId: number;//资源id
+  type?: number;//资源类型0: 歌曲 1: mv2: 歌单3: 专辑4: 电台5: 视频6: 动态
+  commentId?: number;
+  t?: number;//评论类型1: 发送 2: 回复
+  updateCommentList: (comment: any) => void;
 }>(), { commentPlaceholder: '请输入评论', title: '评论', type: 1, commentId: 0, t: 2 });
 
 defineExpose({
@@ -28,7 +28,7 @@ const handleSubmitCommitClick = () => {
   if (!commentContent.value) {
     return window.$message.error('评论不能为空!');
   }
-  let params:any = {
+  let params: any = {
     t: props.t,
     type: props.type,
     id: props.resourceId,
@@ -60,28 +60,15 @@ const handleSubmitCommitClick = () => {
 <template>
   <!-- 回复评论模态框 -->
   <teleport to="body">
-    <n-modal
-      v-model:show="showModal"
-      preset="dialog"
-      :title="title"
-      positive-text="评论"
-      transform-origin="center"
-      :show-icon="false"
-    >
+    <n-modal v-model:show="showModal" preset="dialog" :title="title" positive-text="评论" transform-origin="center"
+      :show-icon="false">
       <div class="my-4 h-32">
-        <n-input
-          v-model:value="commentContent"
-          class="h-full" :placeholder="commentPlaceholder" maxlength="140"
-          show-count
-          type="textarea"
-        />
+        <n-input v-model:value="commentContent" class="h-full" :placeholder="commentPlaceholder" maxlength="140"
+          show-count type="textarea" />
       </div>
       <template #action>
-        <n-button
-          :loading="commentBtnLoading"
-          :disabled="!commentContent.length" type="primary" size="medium"
-          @click="handleSubmitCommitClick"
-        >
+        <n-button :loading="commentBtnLoading" :disabled="!commentContent.length" type="primary" size="medium"
+          @click="handleSubmitCommitClick">
           评论
         </n-button>
       </template>
